359351011f6c7c3856568bcc2db72766e62b1787
unknown <momen07996@gmail.com>
2023-10-07 13:35:53 +0300

    start cards

---
+++ components/cardsSanam.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="mt-5 pt-2 text-center position-relative sectionCardsSanam">
+        <div class="containerNavbar">
+            <div class="d-flex flex-column gap10">
+                <div>
+                    <img src="~/assets/images&icons/imagesAboutUs/imageSanam.png" alt="">
+                </div>
+                <h2 class="text-h4"><span class="activeTitle">سنــام</span> منصة شاملة متخصصة في رعاية الإبل</h2>
+                <p>سـنام منصة شاملة متخصصة في رعاية الإبل نوفِّر خدمات ومنتجات متميزة لتلبية احتياجات عملائنا، ونقدِّم
+                    معلومات شاملة ومفيدة لتعزيز الوعي بالإبل ومواكبة الأحداث والفعاليات المتعلقة بها. يتميز مشروعنا بتركيزنا
+                    على دمج التراث العربي المرتبط بالإبل بالتقنيات الحديثة، لتحقيق التوازن بين الحفاظ على التراث وتطوير مجال
+                    رعاية الإبل بشكل مستدام ومبتكر.</p>
+            </div>
+            <v-row class="mt-5 pt-5">
+                <v-col>
+                    <div class="divCard px-5 py-5 d-flex flex-column align-center gap10">
+                        <div class="divCardImage rounded-circle d-flex justify-center align-center">
+                            <img src="~/assets/images&icons/imagesAboutUs/iconAboutUs1.svg" alt="">
+                        </div>
+                        <div class="d-flex flex-column gap10 divHoverTop">
+                            <h2>الأهداف</h2>
+                            <p class="textCard sizeText14">نتجاوز الحدود التقليدية ونجسد الروح الإبداعية في مجال رعاية
+                                الإبل. نسعى جاهدين لتعزيز التراث العربي ونشر الوعي بأهمية الإبل من خلال منصة شاملة ومتقدمة
+                                تلبي جميع احتياجات الإبل وتضمن رفاهيتها وصحتها.</p>
+                        </div>
+                    </div>
+                </v-col>
+                <v-col>
+                    <div class="divCard px-5 py-5 d-flex flex-column align-center gap10">
+                        <div class="divCardImage rounded-circle d-flex justify-center align-center">
+                            <img src="~/assets/images&icons/imagesAboutUs/iconAboutUs1.svg" alt="">
+                        </div>
+                        <h2>الأهداف</h2>
+                        <p class="textCard sizeText14">نتجاوز الحدود التقليدية ونجسد الروح الإبداعية في مجال رعاية
+                            الإبل. نسعى جاهدين لتعزيز التراث العربي ونشر الوعي بأهمية الإبل من خلال منصة شاملة ومتقدمة
+                            تلبي جميع احتياجات الإبل وتضمن رفاهيتها وصحتها.</p>
+                    </div>
+                </v-col>
+                <v-col>
+                    <div class="divCard px-5 py-5 d-flex flex-column align-center gap10">
+                        <div class="divCardImage rounded-circle d-flex justify-center align-center">
+                            <img src="~/assets/images&icons/imagesAboutUs/iconAboutUs1.svg" alt="">
+                        </div>
+                        <h2>الأهداف</h2>
+                        <p class="textCard sizeText14">نتجاوز الحدود التقليدية ونجسد الروح الإبداعية في مجال رعاية
+                            الإبل. نسعى جاهدين لتعزيز التراث العربي ونشر الوعي بأهمية الإبل من خلال منصة شاملة ومتقدمة
+                            تلبي جميع احتياجات الإبل وتضمن رفاهيتها وصحتها.</p>
+                    </div>
+                </v-col>
+            </v-row>
+        </div>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
     <Header />
     <Main />
-    <div v-for="post in 10" :key="post">
-        <p>welcome</p>
-    </div>
+    <cards-sanam />
 </template>
