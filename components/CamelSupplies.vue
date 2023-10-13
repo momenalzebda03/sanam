16e7841ff0f50806ffc88748cd5202601354ee91
unknown <momen07996@gmail.com>
2023-10-13 11:45:00 +0300

    start section supplies

--- components/CamelSupplies.vue
+++ components/CamelSupplies.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="position-relative sectionWorldOfCamels position-relative sectionSupplies">
+    <section class="sectionWorldOfCamels containerNavbar">
         <component-text-box :propsTextBox="propsTextBox" />
         <swiper :slidesPerView="slidesPerView" :spaceBetween="20" :centeredSlides="true" class="mt-5 pt-5 swiperSupplies">
             <swiper-slide class="swiperText rounded-pill py-1 text-center activeTitle font-weight-bold hoverSwiper"
@@ -8,6 +8,53 @@
                 <span>{{ listSwiper.slideSwiper }}</span>
             </swiper-slide>
         </swiper>
+        <v-row class="mt-5 pt-5 gapRow">
+            <v-col cols="12" md="6" lg="3">
+                <div class="rounded-lg">
+                    <router-link to="/"
+                        class="border d-flex justify-center align-center text-black divCardSupplies position-relative">
+                        <img src="~/assets/images&icons/imagesSupplies/imageHeadband.png" alt="" class="imageHeadband">
+                        <div class="divHoverSupplies d-flex justify-center flex-column gap10">
+                            <div class="divSuppliesBox text-center">
+                                <img src="~/assets/images&icons/imagesSupplies/arrows.png" alt="">
+                                <h2 class="activeTitle">أنقر</h2>
+                                <span class="font-weight-bold">لعرض تفاصيل المنتج</span>
+                            </div>
+                        </div>
+                    </router-link>
+                    <div class="border d-flex justify-space-between align-center py-4 px-4 bg-white divShadowSupplies">
+                        <div class="py-4 px-3 divMoney rounded-lg h-0 d-flex align-center">
+                            <span class="sizeText14">ريال 25.50</span>
+                        </div>
+                        <div class="d-flex flex-column text-end">
+                            <span>عقال الإبل</span>
+                            <span class="textSuppliesBox">يوضع العقال على الإبل مـ ...</span>
+                        </div>
+                    </div>
+                </div>
+            </v-col>
+            <v-col cols="12" md="6" lg="3">
+                <p>welcome</p>
+            </v-col>
+            <v-col cols="12" md="6" lg="3">
+                <p>welcome</p>
+            </v-col>
+            <v-col cols="12" md="6" lg="3">
+                <p>welcome</p>
+            </v-col>
+            <v-col cols="12" md="6" lg="3">
+                <p>welcome</p>
+            </v-col>
+            <v-col cols="12" md="6" lg="3">
+                <p>welcome</p>
+            </v-col>
+            <v-col cols="12" md="6" lg="3">
+                <p>welcome</p>
+            </v-col>
+            <v-col cols="12" md="6" lg="3">
+                <p>welcome</p>
+            </v-col>
+        </v-row>
     </section>
 </template>
 
