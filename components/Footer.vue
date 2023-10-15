4c1002fb221ff9d4d9f78efe5162719c389d4315
unknown <momen07996@gmail.com>
2023-10-15 20:12:08 +0300

    start section footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="containerNavbar mt-5 pt-5">
+    <section class="sectionCenter containerNavbar mt-5 pt-5 position-absolute w-100">
         <div class="sectionFooterCenter rounded-xl text-white mt-5 py-5">
             <v-container>
                 <v-row class="flex-column-reverse flex-md-row align-center">
@@ -16,14 +16,14 @@
                                         </label>
                                     </div>
                                 </v-col>
-                                <v-col cols="12" md="4">
+                                <v-col cols="12" md="3">
                                     <input type="submit" class="activeSwiper w-100 py-5 rounded-pill inputSubmit"
                                         value="اشتراك">
                                 </v-col>
                             </v-row>
-                            <div class="d-flex text-end me-5 mt-5">
-                                <label>أوافق على إستلام النشرة البريدية لمنصة سنام </label>
-                                <input type="checkbox">
+                            <div class="d-flex me-3 mt-5 justify-center justify-md-end align-center gap10">
+                                <label for="inputCheck" class="mt-1">أوافق على إستلام النشرة البريدية لمنصة سنام </label>
+                                <input type="checkbox" id="inputCheck">
                             </div>
                         </form>
                     </v-col>
@@ -38,13 +38,110 @@
             </v-container>
         </div>
     </section>
+    <!-- start footer -->
+    <section class="sectionFooter">
+        <v-container>
+            <v-row>
+                <v-col cols="6" md="3" class="text-end">
+                    <h2 class="font-weight-bold activeTitle py-5 rounded-xl">خدمات الإبل</h2>
+                    <ul class="d-md-flex flex-column rowGap1 d-none">
+                        <li>الخدمات الإستشارية</li>
+                        <li>خدمات نقل الإبل</li>
+                        <li>خدمات العناية والنظافة</li>
+                        <li>خدمات طبية</li>
+                    </ul>
+                </v-col>
+                <v-col cols="6" md="3" class="text-end">
+                    <h2 class="font-weight-bold activeTitle py-5 rounded-xl">لوازم الإبل</h2>
+                    <ul class="d-md-flex flex-column rowGap1 d-none">
+                        <li>منتجات العناية والنظافة </li>
+                        <li>منتجات غذاء الإبل</li>
+                        <li>منتجات الرعاية الصحية</li>
+                        <li>منتجات واكسسوارات الزينة</li>
+                    </ul>
+                </v-col>
+                <v-col cols="6" md="3" class="text-end">
+                    <h2 class="font-weight-bold activeTitle py-5 rounded-xl">عالم الإبل</h2>
+                    <ul class="d-md-flex flex-column rowGap1 d-none">
+                        <li>الإبل والتاريخ</li>
+                        <li>الإبل والثقافة</li>
+                        <li>الإبل والفنون</li>
+                        <li>الإبل والاقتصاد</li>
+                    </ul>
+                </v-col>
+                <v-col cols="6" md="3" class="text-end">
+                    <h2 class="font-weight-bold activeTitle py-5 rounded-xl">من سنام</h2>
+                    <ul class="d-md-flex flex-column rowGap1 d-none">
+                        <li>قصتنا وأهدافنا</li>
+                        <li>رؤيتنا وقيمتنا</li>
+                        <li>تواصل معنا</li>
+                    </ul>
+                </v-col>
+            </v-row>
+        </v-container>
+        <div class="containerNavbar my-5">
+            <hr>
+            <v-row class="mt-3 align-center flex-column flex-md-row">
+                <v-col class="d-md-none">
+                    <div class="justify-center justify-md-end d-flex gap25">
+                        <router-link to="/" class="text-h6 text-decoration-none text-black">
+                            الشروط والأحكام
+                        </router-link>
+                        <router-link to="/" class="text-h6 text-decoration-none text-black">
+                            الشروط والأحكام
+                        </router-link>
+                    </div>
+                </v-col>
+                <v-col>
+                    <ul class="d-flex justify-md-start justify-center gap10">
+                        <li>
+                            <router-link to="/">
+                                <img src="~/assets/images&icons/imagesFooter/iconFacebook.svg" alt="">
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/">
+                                <img src="~/assets/images&icons/imagesFooter/iconInsgrame.svg" alt="">
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/">
+                                <img src="~/assets/images&icons/imagesFooter/iconTwiiter.svg" alt="">
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/">
+                                <img src="~/assets/images&icons/imagesFooter/iconLinkin.svg" alt="">
+                            </router-link>
+                        </li>
+                    </ul>
+                </v-col>
+                <v-col>
+                    <div class="text-center">
+                        <span class="text-h6">جميع الحقوق محفوظة © 2023</span>
+                    </div>
+                </v-col>
+                <v-col class="d-none d-md-block">
+                    <div class="justify-center justify-md-end d-flex gap25">
+                        <router-link to="/" class="text-h6 text-decoration-none text-black">
+                            سياسات الخصوصية
+                        </router-link>
+                        <router-link to="/" class="text-h6 text-decoration-none text-black">
+                            الشروط والأحكام
+                        </router-link>
+                    </div>
+                </v-col>
+            </v-row>
+        </div>
+    </section>
+    <!-- end footer -->
 </template>
 
 <script setup>
 const loopInputs = ref([
     { inputlastName: '', scrrenMd: 6, type: 'text', textLabel: 'الأسم الاول' },
     { inputlastName: '', scrrenMd: 6, type: 'text', textLabel: 'الأسم الاخير' },
-    { inputlastName: '', scrrenMd: 8, type: 'email', textLabel: 'البريد الاكتروني' }
+    { inputlastName: '', scrrenMd: 9, type: 'email', textLabel: 'البريد الاكتروني' }
 ]);
 
 loopInputs.value.forEach((input) => {
