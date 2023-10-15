525aa4fe416cc1a5edbe6a846d92577c8b74668f
unknown <momen07996@gmail.com>
2023-10-15 16:46:49 +0300

    end section inpt

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,39 +5,29 @@
                 <v-row class="flex-column-reverse flex-md-row align-center">
                     <v-col cols="12" md="8">
                         <form>
-                            <v-row>
-                                <v-col cols="12" md="6">
-                                    <div class="form__group field position-relative">
-                                        <input type="text" ref="lastNameInput"
-                                            class="w-100 form__field rounded-pill bg-white py-5 px-5 text-end" required />
-                                        <label class="form__label position-absolute"
-                                            @click="focusInput($refs.lastNameInput)"> الاسم الاخير </label>
+                            <v-row class="flex-row-reverse">
+                                <v-col cols="12" :md="loopInput.scrrenMd" v-for="loopInput in loopInputs"
+                                    :key="loopInput.textLabel">
+                                    <div class="position-relative">
+                                        <input :type="loopInput.type" v-model="loopInput.inputlastName"
+                                            class="w-100 rounded-pill bg-white py-5 px-5 text-end" required />
+                                        <label class="formLabel position-absolute"
+                                            :class="{ 'activeLabel': loopInput.hasText }"> {{ loopInput.textLabel }}
+                                        </label>
                                     </div>
                                 </v-col>
-                                <v-col cols="12" md="6">
-                                    <div class="form__group field position-relative">
-                                        <input type="text" ref="firstNameInput"
-                                            class="w-100 form__field rounded-pill bg-white py-5 px-5 text-end" required />
-                                        <label class="form__label position-absolute"
-                                            @click="focusInput($refs.firstNameInput)"> الاسم الأول </label>
-                                    </div>
-                                </v-col>
-                                <v-col cols="12" md="3">
+                                <v-col cols="12" md="4">
                                     <input type="submit" class="activeSwiper w-100 py-5 rounded-pill inputSubmit"
-                                        value="اشتراك" />
-                                </v-col>
-                                <v-col cols="12" md="9">
-                                    <div class="form__group field position-relative">
-                                        <input type="email" ref="lastEmailInput"
-                                            class="w-100 form__field rounded-pill bg-white py-5 px-5 text-end" required />
-                                        <label class="form__label position-absolute"
-                                            @click="focusInput($refs.lastEmailInput)">البريد الألكتروني</label>
-                                    </div>
+                                        value="اشتراك">
                                 </v-col>
                             </v-row>
+                            <div class="d-flex text-end me-5 mt-5">
+                                <label>أوافق على إستلام النشرة البريدية لمنصة سنام </label>
+                                <input type="checkbox">
+                            </div>
                         </form>
                     </v-col>
-                    <v-col cols="12" md="4">
+                    <v-col cols=" 12" md="4">
                         <div class="d-flex flex-column gap10 text-center text-md-end">
                             <h2>آخر الأخبار والتحديثات</h2>
                             <span>اشترك بالنشرة الإخبارية في سنام للحصول على أحدث الأخبار والتحديثات في عالم الإبل ومواعييد
@@ -51,8 +41,13 @@
 </template>
 
 <script setup>
-const focusInput = (inputRef) => {
-    inputRef.focus();
-    console.log("Input value:", lastNameInput); // Access and log the input's value
-}
+const loopInputs = ref([
+    { inputlastName: '', scrrenMd: 6, type: 'text', textLabel: 'الأسم الاول' },
+    { inputlastName: '', scrrenMd: 6, type: 'text', textLabel: 'الأسم الاخير' },
+    { inputlastName: '', scrrenMd: 8, type: 'email', textLabel: 'البريد الاكتروني' }
+]);
+
+loopInputs.value.forEach((input) => {
+    input.hasText = computed(() => input.inputlastName.length > 0);
+});
 </script>
